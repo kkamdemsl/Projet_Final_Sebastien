5ce16369354b558c208e3da3a0afa7926b39d53d
sebastien <sebastien@gmail.com>
2024-01-23 17:57:53 -0500

    Initial commit

---
+++ src/main/java/com/example/projet_sebastien/ProjetSebastienApplication.java
@@ -0,0 +1,13 @@
+package com.example.projet_sebastien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetSebastienApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetSebastienApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projet_sebastien/ProjetSebastienApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet_sebastien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetSebastienApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
